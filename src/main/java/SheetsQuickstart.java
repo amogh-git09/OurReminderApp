7ef642924ce432c5e9ebcb618ec699a6dc8ca273
Amogh Rathore <amoghdroid09@gmail.com>
2016-08-24 21:22:55 +0900

    Put dirty details in helper class

--- src/main/java/SheetsQuickstart.java
+++ src/main/java/SheetsQuickstart.java
@@ -1,85 +1,20 @@
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.json.JsonFactory;
 import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.*;
 import com.google.api.services.sheets.v4.Sheets;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class SheetsQuickstart {
-  private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
-  private static final java.io.File DATA_STORE_DIR = new java.io.File(System.getProperty("user.home"), ".credentials/sheets.googleapis.com-java-quickstart");
-  private static FileDataStoreFactory DATA_STORE_FACTORY;
-  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-  private static HttpTransport HTTP_TRANSPORT;
-  private static final List<String> SCOPES = Arrays.asList(SheetsScopes.SPREADSHEETS);
-
-  static {
-    try {
-      HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-      DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-    } catch (Throwable t) {
-      t.printStackTrace();
-      System.exit(1);
-    }
-  }
-
-  public static Credential authorize() throws IOException {
-    //Load client secrets
-    InputStream in = SheetsQuickstart.class.getResourceAsStream("/client_secret.json");
-    GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-    GoogleAuthorizationCodeFlow flow =
-      new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-      .setDataStoreFactory(DATA_STORE_FACTORY)
-      .setAccessType("offline")
-      .build();
-
-    Credential credential = new AuthorizationCodeInstalledApp(
-        flow, new LocalServerReceiver()).authorize("user");
-
-    System.out.println("Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-    return credential;
-  }
-
-  public static Sheets getSheetsService() throws IOException {
-    Credential credential = authorize();
-    return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-        .setApplicationName(APPLICATION_NAME)
-        .build();
-  }
+  public static final String SPREADSHEET_ID = "15gZhgEnynebN8_IMrCiXL2QFIMPq2OnC8Ni0eVuy0Po";
+  public static final String CURRENT_RANGE_LOCATION = "C2:C2";
 
   public static void main(String[] args) throws IOException {
-    Sheets service = getSheetsService();
-    String spreadsheetId = "15gZhgEnynebN8_IMrCiXL2QFIMPq2OnC8Ni0eVuy0Po";
-    String range = "C2:C2";
-    ValueRange response = service.spreadsheets().values()
-          .get(spreadsheetId, range)
-          .execute();
-    List<List<Object>> values = response.getValues();
-    String writeRange = "";
-    if(values == null || values.size() == 0){
-      System.out.println("No data found.");
-    } else {
-      for(List row: values) {
-        writeRange = row.get(0).toString();
-        System.out.printf("%s\n", row.get(0));
-      }
-    }
+    Sheets service = SheetsHelper.getSheetsService();
+
+    List<List<Object>> values = SheetsHelper.getValues(service, SPREADSHEET_ID, CURRENT_RANGE_LOCATION);
+    String writeRange = values.get(0).get(0).toString();
 
     Request appendRequest = new Request();
     appendRequest.set("ValueInputOption", "USER_ENTERED");
@@ -94,7 +29,7 @@ public class SheetsQuickstart {
     toWrite.setRange(writeRange);
     toWrite.setValues(data);
     AppendValuesResponse res = service.spreadsheets().values()
-            .append(spreadsheetId, writeRange, toWrite)
+            .append(SPREADSHEET_ID, writeRange, toWrite)
             .set("valueInputOption", "USER_ENTERED")
             .execute();
 
@@ -110,7 +45,7 @@ public class SheetsQuickstart {
     toWrite.setRange("C2:C2");
     toWrite.setValues(data);
     service.spreadsheets().values()
-         .update(spreadsheetId, "C2:C2", toWrite)
+         .update(SPREADSHEET_ID, "C2:C2", toWrite)
          .set("valueInputOption", "USER_ENTERED")
          .execute();
   }
